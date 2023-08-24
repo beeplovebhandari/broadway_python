4ae66cd84eee8e4fab4b54f2b295d0e9b533b455
Beeplove <bbhandari0304@gmail.com>
2023-08-24 10:43:17 +0545

    python 900 code

---
+++ COMMAND FOR GIT .py
@@ -0,0 +1,13 @@
+#git init
+#git status
+#git.add
+#git commit -m "python"
+
+# error aayo vane
+
+#git config --global user.name "aafno username lekhne"
+#git config --global user.email "aafno email lekhne"
+#git commit -m "euta kunai command/name lekhne"
+
+# error aayena vane sakkyo 
+# error aayo vane chai teha github ma diyeko kura haru copy paste hanne 
